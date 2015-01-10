028f3afa8e3a47387c0ad220fb41db1b340739e8
Andrew Newdigate <andrew@troupe.co>
2015-01-10 23:56:14 +0000

    Favourites and recents

--- test/room-collection-test.js
+++ test/room-collection-test.js
@@ -6,6 +6,12 @@ var realtimeClient = require('..');
 var client = new realtimeClient.RealtimeClient({ fayeUrl: 'https://ws-beta.gitter.im/faye', token: process.env.GITTER_TOKEN });
 var rooms = new realtimeClient.RoomCollection([], { client: client, listen: true });
 
+var favs = realtimeClient.filteredRooms.favourites(rooms);
+
+favs.on('add remove reset change', function() {
+  console.log(favs.toJSON());
+});
+
 rooms.on('change:unreadItems', function(model) {
   console.log(model.get('uri') + ' ' + model.get('unreadItems'))
 });
