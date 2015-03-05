1fba7e91522dba4461e595baaf0f2f2a5a5015d6
Andrew Newdigate <andrew@troupe.co>
2015-03-05 15:54:48 +0200

    Better handling of sequence errors

--- test/room-collection-test.js
+++ test/room-collection-test.js
@@ -1,18 +1,34 @@
 'use strict';
 
 var realtimeClient = require('..');
-// require('loglevel').setLevel('trace');
+require('loglevel').setLevel('debug');
 
-var client = new realtimeClient.RealtimeClient({ fayeUrl: 'https://ws-beta.gitter.im/faye', token: process.env.GITTER_TOKEN });
+var blocked = require('blocked');
+blocked(function(ms){
+  if (ms > 0) {
+    console.log('BLOCKED FOR %sms', ms);
+  }
+});
+
+var client = new realtimeClient.RealtimeClient({
+  // fayeUrl: 'https://ws-beta.gitter.im/faye',
+  token: process.env.GITTER_TOKEN,
+  fayeOptions: {
+
+  }
+ });
+
 var rooms = new realtimeClient.RoomCollection([], { client: client, listen: true });
 
 var favs = realtimeClient.filteredRooms.favourites(rooms);
 
 /* Display all the favs, all the time */
 favs.on('add remove reset change', function() {
-  console.log(favs.toJSON());
+  // console.log(favs.toJSON());
+  console.log('change');
 });
 
 rooms.on('change:unreadItems', function(model) {
-  console.log(model.get('uri') + ' ' + model.get('unreadItems'))
+  // console.log(model.get('uri') + ' ' + model.get('unreadItems'))
+  console.log('unread');
 });
